3be448f7961d8fa7ed92f6cabb42b192417790af
Leonard Rodrigues do Vale <151870779+leoRDvale@users.noreply.github.com>
2025-01-26 17:28:48 -0300

    Update Main.java

--- Atividade 13_2/src/Main.java
+++ Atividade 13_2/src/Main.java
@@ -3,6 +3,12 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
 
+        /* Criar uma aplicação para gerar 1000 números aleatórios de -9999 a 9999 e armazená-los  num vetor de 1000 posições. 
+        * Faça a impressão desses números na sua ordem de geração. Crie uma lista duplamente encadeada e vá inserindo os números 
+        * presentes no vetor na lista encadeada. Porém cada número deverá ser colocado na sua posição correta na lista duplamente 
+        * encadeada, ou seja,  serão inseridos em ordem crescente na lista. Imprima lista em ordem crescente e depois em ordem decrescente. 
+        *O vetor deve ser percorrido (o vetor não deve ser ordenado), e o número será inserido na posição correta na lista */
+
         Random rand = new Random();
         int[] vetor = new int[1000];
         ListaDuplamenteEncadeada lista = new ListaDuplamenteEncadeada();
